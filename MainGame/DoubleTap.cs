0451e4f22e3d856825c2e9e5ee6ab56ffe0f8500
SuperDex7 <superdexter7@gmail.com>
2024-01-20 15:01:50 -0500

    b

---
+++ MainGame/DoubleTap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+var tapping : boolean;
+var LastTap : float;
+var tapTime : float;
+
+
+function Update()
+{
+    if (Input.GetKeyDown(KeyCode.LeftShift))
+    {
+        if (!tapping)
+        {
+            tapping = true;
+            SingleTap();
+        }
+        if ((Time.time - LastTap) < tapTime)
+        {
+            Debug.Log("DoubleTap");
+            tapping = false;
+        }
+        LastTap = Time.time;
+    }
+}
+
+public function SingleTap()
+{
+    yield WaitForSeconds(tapTime);
+    if (tapping)
+    {
+        Debug.Log("SingleTap");
+        tapping = false;
+    }
+}
